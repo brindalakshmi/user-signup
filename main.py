7f6982933ca1dc0f1e10b702f640513e7bdfaa1d
Brinda Lakshmi <brindalakshmim@gmail.com>
2019-04-15 11:00:01 -0500

    Finished main.py

--- main.py
+++ main.py
@@ -3,21 +3,21 @@ import os
 import jinja2
 import re
 
-template_dir = os.path.join(os.path.dirname(__file__), "templates")  # creates path to templates file via splicing the current directory path (links to templates directory)
-jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape=True)  # creates jinja2 environment to load templates from template folder
+template_dir = os.path.join(os.path.dirname(__file__), "templates")  
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape=True) 
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
 @app.route("/")
 def index():
-    # encoded_error = request.args.get("error")
+
     return render_template("signup.html") 
 def is_filled(val):
-    if val != "":  # if val == empty string
-        return True  # return True
-    else:  # if val == empty string
-        return False  # return False
+    if val != "":  
+        return True 
+    else:  
+        return False  
 
 def no_whitespace(val):
     whitespace = " "
